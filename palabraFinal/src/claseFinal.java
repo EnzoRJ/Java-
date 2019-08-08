191de0b97f78d6751aeb2040beed374ca7a4398c
EnzoRJ <40775772+EnzoRJ@users.noreply.github.com>
2019-08-07 20:31:31 -0400

    uso final en clases

---
+++ palabraFinal/src/claseFinal.java
@@ -0,0 +1,10 @@
+
+//Clases final no permiten la herencia
+
+public final class claseFinal {
+    public static final int var_Primitivo = 10;
+    public static final Persona var_Persona = new Persona();
+    public final void metodoFinal(){
+
+    }
+}
---
+++ palabraFinal/src/ejemploPalabraFinal.java
@@ -0,0 +1,8 @@
+public class ejemploPalabraFinal {
+    public static void main(String[] args) {
+        claseFinal.var_Persona.setNombre("ENZO");
+        System.out.println(claseFinal.var_Persona.getNombre());
+        claseFinal.var_Persona.setNombre("Andres");
+        System.out.println(claseFinal.var_Persona.getNombre());
+    }
+}
