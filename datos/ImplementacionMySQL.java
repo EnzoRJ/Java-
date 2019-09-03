4c87b7588cfa06c334e46b1e2150d6f568793e6e
EnzoRJ <40775772+EnzoRJ@users.noreply.github.com>
2019-09-02 22:20:48 -0400

    Ejemplo Interfaces

---
+++ datos/ImplementacionMySQL.java
@@ -0,0 +1,13 @@
+package datos;
+
+public class ImplementacionMySQL implements AccesoDatos {
+    @Override
+    public void insertar() {
+        System.out.println("Insertar desde MySQL");
+    }
+
+    @Override
+    public void listar() {
+        System.out.println("Listar desde MySQL");
+    }
+}
