ec8f35430aa4f645bdbaddbf69884ea30c7ab7dd
EnzoRJ <40775772+EnzoRJ@users.noreply.github.com>
2019-09-05 10:29:48 -0400

    Comentario a clase

--- ManejoInterfaces/datos/AccesoDatos.java
+++ ManejoInterfaces/datos/AccesoDatos.java
@@ -1,8 +1,10 @@
+
 package datos;
 
 public interface AccesoDatos {
 
     public static int MAX_REGISTROS = 10;
+    //Métodos solo se declaran 
     public abstract void insertar();
     public abstract void listar();
 
